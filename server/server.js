0b4f7d86b2418c39cd4157336c14ef6fe8d36bd9
ismlslckk <ismlslck@gmail.com>
2024-06-01 19:19:25 +0200

    init commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+
+
+const server = require('http').createServer();
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on('connection', socket => {
+  const id = socket.handshake.query.id
+  socket.join(id)
+
+
+  console.log({id})
+
+  socket.on('send-message', ({ recipients, text }) => {
+    recipients.forEach(recipient => {
+      const newRecipients = recipients.filter(r => r !== recipient)
+      newRecipients.push(id)
+      socket.broadcast.to(recipient).emit('receive-message', {
+        recipients: newRecipients, sender: id, text
+      })
+    })
+  })
+})
+server.listen(5001)
